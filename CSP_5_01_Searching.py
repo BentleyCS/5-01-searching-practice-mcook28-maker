e9a4fc08d02f681ef4b4e966959bc1d93d136840
mcook28-maker <mcook28@bentleyschool.org>
2026-01-19 16:24:16 -0800

    Add files via upload

--- CSP_5_01_Searching.py
+++ CSP_5_01_Searching.py
@@ -1,19 +1,51 @@
+import random
+from random import randint
+import random
 
-def randomSearch(items:list, target) -> int:
-    #Modify the below function such that it takes in a list of items and a target value.
-    #Randomly choose an item from the list and if it isn't the target value try again.
-    #print out the amount of tries it took and return the index of the target value
-    pass
-
-def linearSearch(items:list, target) ->tuple[int,int]:
-    #Modify the below function such that it implements linear search.
-    #Return the index of the target value and the amount of checks it took
-    #if the value is not within the list return -1 as the index.
-    pass
+def randomSearch(items: list, target) -> int:
+    tries = 0
+    while True:
+        tries += 1
+        i = random.randint(0, len(items) - 1)
+        if items[i] == target:
+            print("Found it in", tries, "tries")
+            return i
 
 
-def binarySearch(items:list, target) -> tuple[int,int]:
-    # Modify the below function such that it implements linear search.
-    # Return the index of the target value and the amount of checks it took
-    # if the value is not within the list return -1 as the index.
-    pass
+print(randomSearch([1,3,5,9,7,12,11,10,2,4,6,8], 5))
+
+
+def linearSearch(items: list, target) -> tuple[int, int]:
+    checks = 0
+    for i in range(len(items)):
+        checks += 1
+        if items[i] == target:
+            return i, checks
+    return -1, checks
+
+
+print(linearSearch([1,3,5,9,7,12,11,10,2,4,6,8], 5))
+
+
+def binarySearch(items: list, target) -> tuple[int, int]:
+    low = 0
+    high = len(items) - 1
+    checks = 0
+
+    while low <= high:
+        checks += 1
+        mid = (low + high) // 2
+
+        if items[mid] == target:
+            return mid, checks
+        elif items[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1, checks
+
+
+# IMPORTANT: binary search requires a sorted list
+sorted_items = sorted([1,3,5,9,7,12,11,10,2,4,6,8])
+print(binarySearch(sorted_items, 5))
